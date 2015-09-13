53e2b21b2fb4a82ac6c598cbcf43cc12f9a2e9f1
Integralist <mark.mcdx@gmail.com>
2015-09-13 21:29:08 +0100

    Fix godo task to work with gb directory structure

--- tasks/Godofile.go
+++ tasks/Godofile.go
@@ -4,7 +4,7 @@ import . "gopkg.in/godo.v1"
 
 func tasks(p *Project) {
 	p.Task("watch-server", func(c *Context) error {
-		return Start(`requester.go`)
+		return Start("main.go ../page.yaml", M{"$in": "./src/requester"})
 	}).Watch("**/*.go")
 }
 
